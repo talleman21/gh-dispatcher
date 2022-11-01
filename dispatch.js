356d5d0b67546dc6440b2cfc731522110a22cc7a
T.J. Alleman <talleman21@gmail.com>
2022-11-01 16:09:37 -0700

    add @actions/core

--- dispatch.js
+++ dispatch.js
@@ -1,5 +1,12 @@
+import * as core from '@actions/core'
+
+const inputs = {
+    targetWorkflow: core.getInput('target-workflow'),
+    targetWorkflowGithubAccount: core.getInput('target-workflow-github-account'),
+    targetWorkflowRepo: core.getInput('target-workflow-repo'),
+}
+
 console.log('I am running from dispatch.js')
-const [,,command,workflowType] = process.argv;
-console.log(command, workflowType)
+console.log('inputs', inputs)
 
 console.log('tok', process.env.pat_token)
